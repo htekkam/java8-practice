eb36ccbfb0888cd7534db47b7524f992c5ed8c30
harikrishna t <harikrishna.tekkam@gmail.com>
2025-07-09 09:45:32 +0530

    first commit

---
+++ src/com/java8/features/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,10 @@
+package com.java8.features.lambdas;
+
+public class RunnableLambdaExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author harik
+ *
+ */
+module java8_10hours_tutorial {
+}
